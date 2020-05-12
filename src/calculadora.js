553be21915cc3167f5db4ea48f330b856246e508
unknown <yutasasagawa898@gmail.com>
2020-05-11 21:51:21 -0300

    first commit

---
+++ src/calculadora.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './calculadora.css';
+import { Jumbotron } from 'react-bootstrap';
+
+function Calculadora() {
+  return (
+   <Jumbotron style={{
+    background: 'tranparent !important',
+    backgroundColor: '#007bFF',
+    padding: '5px',
+    margin: '5px',
+    width: '400px',
+   }}>
+
+     </Jumbotron>
+  );
+}
+
+export default Calculadora;
