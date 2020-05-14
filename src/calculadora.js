65a6714578ec794d5be4982e7c866ecca7df84c0
unknown <yutasasagawa898@gmail.com>
2020-05-13 22:59:19 -0300

    funcionalidade da calculadora ok

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,19 +1,45 @@
 import React, { useState } from 'react';
 import './calculadora.css';
 import { Jumbotron, Container, Row, Col, Button, Form } from 'react-bootstrap';
+import CalculadoraService from './calculadora.service';
 
 function Calculadora() {
 
+     const [calcular, concatenarNumero,SOMA, SUBTRACAO, MULTIPLICACAO, DIVISAO] = CalculadoraService();
+
     const [txtNumeros, setTxtNumeros] = useState('0');
+    const [numero1, setNumero1] = useState('0');
+    const [numero2, setNumero2] = useState(null);
+    const [operacao, setOperacao] = useState(null);
 
     function adicionarNumero(numero) {
-
-      setTxtNumeros(txtNumeros + numero);
+      let resultado;
+      if(operacao === null){
+        resultado = concatenarNumero(numero1, numero);
+        setNumero1(resultado);
+      }else{
+        resultado = concatenarNumero(numero2, numero);
+        setNumero2(resultado);
+      }
+      setTxtNumeros(resultado);
 
     }
 
     function definirOperacao(op) {
-      setTxtNumeros(op);
+      // apenas define a operacao caso ela não exista
+      if(operacao === null) {
+        setOperacao(op);
+        return;
+      }
+     
+      // caso a operação estiver definida e numero2 selecinado, relaizar o calculoda operação.
+      if(numero2 !== null){
+        const resultado = calcular(parseFloat(numero1), parseFloat(numero2), operacao);
+        setOperacao(op);
+        setNumero1(resultado.toString());
+        setNumero2(null);
+        setTxtNumeros(resultado.toString());
+      }
 
     }
 
@@ -55,7 +81,7 @@ function Calculadora() {
         </Row>
         <Row>
           <Col><Button variant="light" onClick={() => adicionarNumero('0')} >0</Button></Col>
-          <Col><Button variant="light">.</Button></Col>
+          <Col><Button variant="light" onClick={() => adicionarNumero('.')}>.</Button></Col>
           <Col><Button variant="success">=</Button></Col>
           <Col><Button variant="warning" onClick={() => definirOperacao('+')}>+</Button></Col>
         </Row>
